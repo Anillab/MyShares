0f0930befca37ca101a1b8a70116d4c4ddc9e6e8
Anillab <anillabusolo@gmail.com>
2018-07-03 10:09:12 +0300

    created a  user model which has the users table

--- app/models.py
+++ app/models.py
@@ -1,1 +1,7 @@
 # File for models/classes
+from . import db
+class User(db.Model):
+    """docstring for [object Object]."""
+    __tablename__='users'
+    id=db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(255))
