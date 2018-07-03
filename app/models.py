a7a85f8a85846ab43b7b8d473c9bafb61111187a
Anillab <anillabusolo@gmail.com>
2018-07-03 10:55:31 +0300

    creayed a password secure method for authentication

--- app/models.py
+++ app/models.py
@@ -1,7 +1,17 @@
 # File for models/classes
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 class User(db.Model):
     """docstring for [object Object]."""
     __tablename__='users'
     id=db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+        @property
+        def password(self):
+            raise AttributeError('Failed You cannot read the password')
+        @property.setter
+        def password(self,password):
+            self.pass_secure=generate_password_hash(password)
+        def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
