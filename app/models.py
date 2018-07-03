3186df9938112e7ad9ccc89149b96fa7ce283dd4
Anillab <anillabusolo@gmail.com>
2018-07-03 11:25:15 +0300

    created a Stock_info table with various columns

--- app/models.py
+++ app/models.py
@@ -15,3 +15,18 @@ class User(db.Model):
             self.pass_secure=generate_password_hash(password)
         def verify_password(self,password):
             return check_password_hash(self.pass_secure,password)
+class Stock_Info(db.Model):
+    """docstring for [object Object]."""
+    __tablename__='stocks_info'
+    id=db.Column(db.Integer,primary_key=True)
+    Name=db.Column(db.String(255))
+    Company_id=db.Column(db.String(255))
+    Security_id=db.Column(db.Integer)
+    High_trade=db.Column(db.Integer)
+    Low_trade=db.Column(db.Integer)
+    Last_traded_price=db.Column(db.Integer)
+    Close_price=db.Column(db.Integer)
+    Prev_close=db.Column(db.Integer)
+    Vol_traded=db.Column(db.Integer)
+    Year_low_price=db.Column(db.Integer)
+    Year_high_price=db.Column(db.Integer)
